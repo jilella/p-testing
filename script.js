cdcf7003a16580c68f29ec40e5b074a4462152f7
jilella <141404675+jilella@users.noreply.github.com>
2023-11-29 09:45:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Wait for the DOM to be fully loaded
+<script>
+        function submitForm() {
+            var age = document.getElementById('age').value;
+            var gender = document.getElementById('gender').value;
+            var race = document.getElementById('race').value;
+            var time_in_hospital = document.getElementById('time_in_hospital').value;
+            var glucose_levels = document.getElementById('glucose_levels').value;
+            var patient_nbr = document.getElementById('patient_nbr').value;
 
-    // Get the input element and output element
-    var inputElement = document.getElementById('inputNumber');
-    var outputElement = document.getElementById('output');
-
-    // Add an event listener to the input element
-    inputElement.addEventListener('input', function() {
-        // Convert the input value to a number
-        var inputValue = parseFloat(inputElement.value);
-
-        // Check if the input value is greater than 5
-        if (inputValue > 5) {
-            outputElement.textContent = 'Accepted';
-        } else {
-            outputElement.textContent = 'Declined';
+            // Do something with the collected values, e.g., send to a server
+            console.log("Age: " + age);
+            console.log("Gender: " + gender);
+            console.log("Race: " + race);
+            console.log("Time in Hospital: " + time_in_hospital);
+            console.log("Glucose Levels: " + glucose_levels);
+            console.log("Patient Number: " + patient_nbr);
         }
-    });
-});
+    </script>
