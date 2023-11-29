e7b2cd21a35eb42424271e0b92842397541e5b2d
jilella <141404675+jilella@users.noreply.github.com>
2023-11-29 10:00:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-<script>
+ <script defer>
         function submitForm() {
-            var age = document.getElementById('age').value;
-            var gender = document.getElementById('gender').value;
-            var race = document.getElementById('race').value;
-            var time_in_hospital = document.getElementById('time_in_hospital').value;
-            var glucose_levels = document.getElementById('glucose_levels').value;
-            var patient_nbr = document.getElementById('patient_nbr').value;
-                var result = time_in_hospital > 5 ? 1 : 0;
+            // Get the value of time_in_hospital input
+            var timeInHospital = document.getElementById("time_in_hospital").value;
 
-            console.log("Result: " + result);
+            // Convert the value to a number and check if it's less than 5
+            var result = parseInt(timeInHospital) < 5 ? 1 : 0;
 
-            // Do something with the collected values, e.g., send to a server
-            console.log("Age: " + age);
-            console.log("Gender: " + gender);
-            console.log("Race: " + race);
-            console.log("Time in Hospital: " + time_in_hospital);
-            console.log("Glucose Levels: " + glucose_levels);
-            console.log("Patient Number: " + patient_nbr);
+            // Display the result (you can modify this part based on your requirements)
+            alert("Result: " + result);
         }
     </script>
